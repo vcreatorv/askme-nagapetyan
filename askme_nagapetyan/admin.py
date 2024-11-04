feef006cb04261e0db6103cff3202241980b380d
vcreatorv <valery.nagapetyan@yandex.ru>
2024-11-04 21:06:23 +0300

    registered models in django admin

--- askme_nagapetyan/admin.py
+++ askme_nagapetyan/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Question, Tag, Profile, Answer, QuestionLike, AnswerLike
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Tag)
+admin.site.register(Profile)
+admin.site.register(Answer)
+admin.site.register(QuestionLike)
+admin.site.register(AnswerLike)
